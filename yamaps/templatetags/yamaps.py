72c5d4f1e8f1f2d56d4e412d21b630838a98549a
Dmitry Shlagoff <wessmoke@gmail.com>
2017-07-29 13:53:32 +0700

    Refactored & fixed init script

--- yamaps/templatetags/yamaps.py
+++ yamaps/templatetags/yamaps.py
@@ -15,20 +15,25 @@ def yamaps_init():
     lang = "ru-RU"
     if settings.LANGUAGE_CODE:
         lang = settings.LANGUAGE_CODE
-    yamaps_js = '<script type="text/javascript" src="https://api-maps.yandex.ru/2.1/?lang={}"></script>'\
+    yamaps_js = '<script type="text/javascript" src="https://api-maps.yandex.ru/2.1/?lang={}"></script>' \
         .format(lang)
     return mark_safe(yamaps_js + '''
     <script type="text/javascript">
-    ymaps.ready(function () {
-        var maps = document.querySelectorAll(".django-yamaps");
-        [].forEach.call(maps, function (elem) {
-            var map = new ymaps.Map(elem, {
-                center: [53.758768, 87.136731],
-                controls: ["zoomControl", "fullscreenControl", "searchControl"]
+    window.onload = function() {
+        ymaps.ready(function () {
+            var maps = document.querySelectorAll(".django-yamaps");
+            maps.forEach(function (elem) {
+                var map = new ymaps.Map(elem, {
+                    center: [53.758768, 87.136731],
+                    zoom: 15
+                });
+                var addr = elem.getAttribute("data-raw-address");
+                ymaps.geocode(addr, { results: 1 }).then(function (res) {
+                    var firstGeoObject = res.geoObjects.get(0),
+                        coords = firstGeoObject.geometry.getCoordinates();
+                    map.geoObjects.add(firstGeoObject);
+                    map.setCenter(coords);
+                });
             });
-            var addr = elem.getAttribute("data-raw-address");
-            var searchControl = map.controls.get("searchControl");
-            searchControl.search(addr);            
-        });
-    });
+    })};
     </script>''')
