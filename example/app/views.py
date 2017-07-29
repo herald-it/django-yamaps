ba3873a867849115cd70dc0b7f67926a5d5b9bee
Dmitry Shlagoff <wessmoke@gmail.com>
2017-07-29 13:53:02 +0700

    Using all addresses

--- example/app/views.py
+++ example/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Address
+
+
+def index(request):
+    addresses = Address.objects.all()
+    return render(request, "index.html", {"addresses": addresses})
--- example/example/urls.py
+++ example/example/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from app.views import index
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index),
 ]
